e2f236ef2b812cffebaf14ec108108f2e17f6b95
Edouard Benauw <edouardbenauw@gmail.com>
2025-02-25 15:06:57 +0100

    Time to vigie

---
+++ ai-vigie.py
@@ -0,0 +1,4 @@
+import random
+
+coupables = ['Abdou', 'Antoine', 'Arthur', 'Baptiste', 'Christophe', 'Ian', 'Ismail', 'Jean', 'Julen', 'Julien', 'Louis', 'Mouna', 'Oumayma', 'Pierre', 'Rudy', 'Saïfane', 'Simon', 'ce mec qui est parti il y a longtemps']
+print(f'C\'est la faute de {random.choice(coupables)} et de sa PR qui a été mergée sans tester !')
